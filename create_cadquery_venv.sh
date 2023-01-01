75033b84f9c7c78979ae64f895c55b7de9949387
Boris-Chengbiao Zhou <bobo1239@web.de>
2023-01-01 21:45:05 +0100

    Initial commit

---
+++ create_cadquery_venv.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+rm -rf CQ-editor
+git clone git@github.com:CadQuery/CQ-editor.git
+# Apply patch from https://github.com/CadQuery/CQ-editor/pull/357#issuecomment-1294051468
+cd CQ-editor && git apply ../cq-editor-pip.diff && cd ..
+
+rm -rf cadquery_venv
+python3 -m venv cadquery_venv
+source cadquery_venv/bin/activate
+pip install --use-pep517 --pre git+https://github.com/CadQuery/cadquery.git
+pip install --use-pep517 ./CQ-editor
