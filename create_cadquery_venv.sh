a9cc77294fea7f80e548d9c81cadd1f4d492a524
Boris-Chengbiao Zhou <bobo1239@web.de>
2023-05-16 02:33:16 +0200

    Add workaround for Python 3.11 compat

--- create_cadquery_venv.sh
+++ create_cadquery_venv.sh
@@ -8,6 +8,13 @@ cd CQ-editor && git apply ../cq-editor-pip.diff && cd ..
 rm -rf cadquery_venv
 python3 -m venv cadquery_venv
 source cadquery_venv/bin/activate
+pip install --upgrade pip
 pip install wheel
+
+# TODO: Remove once https://github.com/DanielBok/nlopt-python/issues/19 is resolved
+pip install swig
+pip install numpy
+pip install git+https://github.com/DanielBok/nlopt-python.git@develop/3.11
+
 pip install git+https://github.com/CadQuery/cadquery.git
 pip install ./CQ-editor
